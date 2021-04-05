613093bfa0884834a63dc4a6f08af4affcbe7353
Glimpz <abgabbard@gmail.com>
2021-04-05 15:03:45 -0700

    added axios packageand added style to components. Axios API call successfully rendering

--- etracker/src/App.js
+++ etracker/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Users from "./components/Users";
 
 function App() {
   return (
     <div>
       <Header />
       <Navbar />
+      <Users />
     </div>
   );
 }
