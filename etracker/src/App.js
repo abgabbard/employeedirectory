ad9305c890e52181ee6b8cc1e4001cbb390b269a
Glimpz <abgabbard@gmail.com>
2021-04-05 14:27:05 -0700

    copied react files from class activity 4 for basic header navbar and card

---
+++ etracker/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
